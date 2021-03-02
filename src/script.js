643a073a7f873193649f35633e3fc86461eb57ae
annagaudencio <79924300+annagaudencio@users.noreply.github.com>
2021-03-02 16:41:36 -0300

    Add files via upload

---
+++ src/script.js
@@ -0,0 +1,42 @@
+const WeekView = Vue.component("week-view", {
+  template: `
+<div>
+  <div class="container">
+    <ul class="grid day-grid">
+      <li v-for="day in days" class="day-grid-item"> {{ day }} </li>
+    </ul>
+    <ul class="grid date-grid">
+      <li class="date-grid-item" v-for="date in week" :class="date === today ? 'highlight': ''"> {{ date }}</li>
+    </ul>
+  </div>
+</div>
+  `,
+  props: ["week", "days", "month", "today"]
+});
+
+new Vue({
+  el: "#app",
+  data: {
+    month: "",
+    week: [],
+    days: ["Mon", "Tues", "Wed", "Thu", "Fri", "Sat", "Sun"],
+    today: 0
+  },
+  methods: {
+    generateWeek() {
+      let date = new Date();
+      this.today = date.getDate();
+      for (let i = 1; i <= 7; i++) {
+        let first = date.getDate() - date.getDay() + i;
+        this.week.push(first);
+      }
+    }
+  },
+  component: {
+    WeekView
+  },
+  mounted() {
+    this.month = new Date().getMonth() + 1;
+    this.generateWeek();
+  }
+});
